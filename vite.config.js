ecabae933985ee47ff1872082542ba679de7262b
Ruben <dimeruben444@gmail.com>
2026-04-27 10:16:43 +0200

    feat(structure) point 0

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,20 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        ej1: 'ej1.html',
+        ej2: 'ej2.html',
+        ej3: 'ej3.html',
+        ej4: 'ej4.html',
+        ej5: 'ej5.html',
+        ej6: 'ej6.html',
+        ej7: 'ej7.html',
+        ej8: 'ej8.html',
+        ej9: 'ej9.html',
+      }
+    }
+  }
 })
